089815515df58a8c6618abc44e2257eb6b937894
Sol <solomon.beer1@gmail.com>
2022-09-19 11:43:16 +0100

    Find max update -Sol

--- day1/functions_recap.py
+++ day1/functions_recap.py
@@ -1,20 +1,38 @@
 import sys
 
 
-def calculate_geometric_series(a, r, n=10):
-    """Calculate the sum of a geometric series"""
-    if r == 1:
-        return a * (n + 1)
-    return a * (1 - r ** (n + 1)) / (1 - r)
+# def calculate_geometric_series(a, r, n=10):
+#     """Calculate the sum of a geometric series"""
+#     if r == 1:
+#         return a * (n + 1)
+#     return a * (1 - r ** (n + 1)) / (1 - r)
+
+def find_max(list):
+    maximum = None
+    first = False
+    for value in list:
+        if first == False:
+            maximum = value
+            first = True
+        if value > maximum:
+            maximum = value
+
+    return maximum
 
 
 def main():
-    a = int(input("a: "))
-    r = float(input("r: "))
-    n = int(input("n: "))
-    print(f"s_n = {calculate_geometric_series(a, r, n)}")
+    #a = int(input("a: "))
+    #r = float(input("r: "))
+    #n = int(input("n: "))
+    #print(f"s_n = {calculate_geometric_series(a, r, n)}")
+    my_list = [1, 7, 9, 42]
+    my_max = find_max(my_list)
+    print(my_max)
     return 0
 
 
 if __name__ == "__main__":
     sys.exit(main())
+
+#Can use flowcharts to cleanly show functions purpose / workflow
+
